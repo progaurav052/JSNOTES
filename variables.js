f69c6f2405bbc55a3a3c829152499a31754d4f05
Gaurav <gaurav511pai@gmail.com>
2024-07-27 11:35:43 +0530

    covered variables lesson

---
+++ variables.js
@@ -0,0 +1,25 @@
+/* there are 3 types of variables 
+let
+const
+var 
+*/
+let x;
+x=100;
+console.log(x);
+console.log(`you are ${x} years old`);
+
+console.log(typeof x)
+
+
+let name="gaurav"
+console.log(typeof name);
+
+//let is block scope
+//var is global scope 
+// const is const
+
+
+
+
+
+
