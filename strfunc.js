f82d06e9e1fb1706a0f93f3b48e0b1dc9661b394
Gaurav <gaurav511pai@gmail.com>
2024-07-27 12:05:41 +0530

    learn str functions

---
+++ strfunc.js
@@ -0,0 +1,14 @@
+/*
+.charAt()
+.indexOf()
+.toLowerCase()
+.toUpperCase()
+.tostartsWith()
+.toendsWith()
+.includes()
+.replaceAll("","")
+*/
+/*
+  .slice(start,end)
+  .slice(-1) //gives 1 character from back
+*/
