db29d61a0e353b0f85f7cdddbde48cf37c894979
Gaurav <gaurav511pai@gmail.com>
2024-07-27 11:46:57 +0530

    added type conversion

---
+++ typecon.js
@@ -0,0 +1,7 @@
+// can be done using Number(),String(),Boolean()
+
+let age=window.prompt("emter the age");
+age=Number(age);
+console.log(age);
+
+//similarly can use Number(),String(), and Boolean()
